c5fa0dc9439a87ea73aac73533549960264ba08e
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-01-05 11:42:19 +0530

    vercel config added

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,22 @@ import App from "./App.jsx";
 import { ConfigProvider } from "antd";
 import theme from "./config/theme.js";
 import Store from "./context/index.jsx";
-
+// import CookieConsent from "react-cookie-consent";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ConfigProvider theme={theme}>
     <Store>
       <App />
+      {/* <CookieConsent
+        location="top"
+        buttonText="Sure man!!"
+        cookieName="connect.sid"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={365}
+      >
+        Accept Cookie Please
+      </CookieConsent> */}
     </Store>
   </ConfigProvider>
 );
